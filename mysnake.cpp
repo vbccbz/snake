b04448e76ea25d7bf399cf9283d670bb880835c4
George <31924134+vbccbz@users.noreply.github.com>
2022-01-10 19:10:48 +0300

    Add files via upload

---
+++ mysnake.cpp
@@ -0,0 +1,33 @@
+#if 0
+50
+25
+50
+25
+500
+200
+10 x 20
+#endif
+
+#include <conio.h>
+#include <windows.h>
+//#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+int main(void) {
+  char c;
+  while (1) {
+    //  c = _getch();
+    //  std::cout << (int)c << '\n';
+    for (size_t i = 0; i < 25; i++) {
+      //printf("%2d ", i);
+      std::cout << std::setw(2) << i << ' ';
+      for (size_t i = 0; i < 50; i++) {
+        std::cout << "*";
+      }
+      std::cout << '\n';
+    }
+    Sleep(500);  // sleep(10);
+    system("cls");
+  }
+  return 0;
+}
