decae1c85a868b8b3dde4d14ca4ab8f6a99ba60d
George <31924134+vbccbz@users.noreply.github.com>
2022-08-22 18:16:39 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 
+
 class TimeGame
 {
 private:
@@ -31,42 +32,66 @@ public:
 		return float(seconds - tmp);
 	}
 };
+class Sprite
+{
+public:
+	char symbol;
+	int row;
+	int column;
+public:
+	Sprite(char s) : symbol(s)
+	{
+		row = 0;
+		column = 0;
+	}
+	void setPosition(int r, int c)
+	{
+		row = r; column = c;
+	}
+	void setSymbol(char ch)
+	{
+		symbol = ch;
+	}
+};
 class Window {
 private:
-	int tick;
-
+	//int tick;
 public:
-	char buffer[1000];
+	char buffer[100];
 	Window(void)
 	{
-		memset(buffer, '.', 100);
-		tick = 0;
+		//tick = 0;
+		clear();
 	}
 	~Window(void)
 	{
 		;
+	}
+	void clear(void)
+	{
+		memset(buffer, '8', 100);
+		system("cls");
+	}
+	void draw(Sprite& s)
+	{
+		buffer[s.row * 10 + s.column] = s.symbol;
 	}
 	void display(void)
 	{
 		//std::cout << "tick " << tick << '\n';
 		//tick += 1;
 
-		for (size_t row = 0; row < 10; ++row)
+		for (size_t i = 0; i < 100; ++i)
 		{
-			for (size_t column = 0; column < 10; ++column)
+			std::cout << buffer[i];
+			if (((i + 1) % 10) == 0)
 			{
-				std::cout << buffer[row * 10 + column];
-			};
-			std::cout << '\n';
+				std::cout << '\n';
+			}
 		}
+		std::cout << '\n';
 
-		//Sleep(100); // flickering (
-	}
-	void clear(void) {
-		system("cls");
-	}
-	void draw(void) {
-		;
+		Sleep(50); // flickering (
 	}
 };
 class Keyboard {
@@ -80,7 +105,8 @@ public:
 		down,
 		any,
 		esc,
-		none
+		none,
+		p,
 	} key;
 	Keyboard(void)
 	{
@@ -110,6 +136,9 @@ public:
 			case 27: // ESC
 				key = keys::esc;
 				break;
+			case 'p': // ESC
+				key = keys::p;
+				break;
 			default:
 				key = keys::any;
 				break;
@@ -120,41 +149,19 @@ public:
 		}
 	}
 };
-class World {
-public:
-	char* table;
-	size_t weight;
-	size_t height;
-
-public:
-	World(size_t w, size_t h)
-		: weight(w)
-		, height(h)
-	{
-		table = new char[weight * height];
-		memset(table, '-', weight * height);
-	}
-	~World(void)
-	{
-		delete[] table;
-	}
-	void draw(Window& window)
-	{
-		for (size_t i = 0; i < 100; i++) {
-			window.buffer[i] = table[i];
-		}
-	}
-};
 class Snake {
-	friend class Window;
-
 private:
-	struct element {
-		struct coordinate {
+	//friend class Window;
+
+public:
+	struct element
+	{
+		struct coordinate
+		{
 			int row;
 			int column;
 		} position;
-		int number;
+		//int number;
 	};
 	element body[100];
 	size_t length;
@@ -164,53 +171,21 @@ private:
 		up,
 		down
 	} direction;
-
+	int life;
+	int hit;
 public:
-	Snake(World& world)
+	Snake(void)
 	{
-		for (size_t element = 0; element < 100; element++) {
-			body[element].position.row = -1;
-			body[element].position.column = -1;
-			body[element].number = element;
-		}
+		life = 3;
 		direction = direction::right;
-		length = 1;
-		// length = 2;
-		body[0].number = 0;
-		// body[1].number = 1;
-		body[0].position.row = 5;
-		// body[1].position.row = 5;
-		body[0].position.column = 5;
-		// body[1].position.column = 4;
-
-#ifdef DEBUG
-		length = 5;
-		body[0].number = 0;
-		body[1].number = 1;
-		body[2].number = 2;
-		body[3].number = 3;
-		body[4].number = 4;
-
-		body[0].position.row = 5;
-		body[1].position.row = 5;
-		body[2].position.row = 5;
-		body[3].position.row = 5;
-		body[4].position.row = 5;
-
-		body[0].position.column = 5;
-		body[1].position.column = 4;
-		body[2].position.column = 3;
-		body[3].position.column = 2;
-		body[4].position.column = 1;
-#endif
-		world.table[body[0].position.row * 10 + body[0].position.column] = 'S';
-		for (size_t element = 1; element < length; element++) {
-			world
-				.table[body[element].position.row * 10 + body[element].position.column]
-				= 's';
+		int r = 0;
+		int c = 5;
+		length = 4;
+		for (int i = 0; i < length; i++)
+		{
+			body[i].position.row = r;
+			body[i].position.column = c--; //body[c].position.row
 		}
-
-		return;
 	};
 	~Snake(void)
 	{
@@ -238,179 +213,125 @@ public:
 			break;
 		}
 	}
-	void move(World& world)
-	{
-		// set head position
-		element temp = body[0];
-		switch (direction)
-		{
-		case direction::up:
-			temp.position.row -= 1;
-			if (temp.position.row < 0) {
-				temp.position.row = 9;
-			}
-			break;
-		case direction::down:
-			temp.position.row += 1;
-			if (temp.position.row > 9) {
-				temp.position.row = 0;
-			}
-			break;
-		case direction::left:
-			temp.position.column -= 1;
-			if (temp.position.column < 0) {
-				temp.position.column = 9;
-			}
-			break;
-		case direction::right:
-			temp.position.column += 1;
-			if (temp.position.column > 9) {
-				temp.position.column = 0;
-			}
-			break;
-		default:
-			break;
-		}
-
-		// interacting with world
-		switch (world.table[temp.position.row * 10 + temp.position.column])
-		{
-		case '-':
-			// clear
-			world.table[body[length - 1].position.row * 10 + body[length - 1].position.column] = '-';
-
-			update();
-			body[0].position = temp.position;
-
-			world.table[body[0].position.row * 10 + body[0].position.column] = 'S';
-			for (size_t element = 1; element < length; element++) {
-				world.table[body[element].position.row * 10 + body[element].position.column] = 's';
-			}
-
-			break;
-
-		case '$':
-			// no clear
-
-			length += 1;
-			update();
-			body[0].position = temp.position;
-
-			world.table[body[0].position.row * 10 + body[0].position.column] = 'S';
-			for (size_t element = 1; element < length; element++) {
-				world.table[body[element].position.row * 10 + body[element].position.column] = 's';
-			}
-
-			break;
-
-		case 's':
-			// clear
-			for (size_t element = 0; element < length; element++) {
-				world.table[body[element].position.row * 10 + body[element].position.column] = '-';
-			}
-
-			// find intersection
-			for (size_t element = 1; element < length; element++) {
-				if (temp.position.row == body[element].position.row && temp.position.column == body[element].position.column) {
-					length = body[element].number + 1;
-				}
-			}
-
-			update();
-			body[0].position = temp.position;
-
-			world.table[body[0].position.row * 10 + body[0].position.column] = 'S';
-			for (size_t element = 1; element < length; element++) {
-				world.table[body[element].position.row * 10 + body[element].position.column] = 's';
-			}
-
-			break;
-
-		default:
-
-			break;
-		}
-
-	}
-	//void Snake::write(World& world)
+	//void move(void)
 	//{
+	//	updateBody();
+	//	moveHead();// otherwise the head must be copied
 	//}
-
-private:
-	void update(void)
+	//void moveHead(void)
+	//{
+	//	switch (direction)
+	//	{
+	//	case direction::up:
+	//		body[0].position.row -= 1;
+	//		break;
+	//	case direction::down:
+	//		body[0].position.row += 1;
+	//		break;
+	//	case direction::left:
+	//		body[0].position.column -= 1;
+	//		break;
+	//	case direction::right:
+	//		body[0].position.column += 1;
+	//		break;
+	//	default:
+	//		break;
+	//	}
+	//}
+	void updateBody(void)
 	{
 		element temp1, temp2;
 		temp1.position = body[0].position;
-		for (size_t element = 1; element < length; ++element) {
-			temp2.position = body[element].position;
-			body[element].position = temp1.position;
+		for (size_t i = 1; i <= length; ++i) {
+			temp2.position = body[i].position;
+			body[i].position = temp1.position;
 			temp1.position = temp2.position;
 		}
 	}
 };
 class Fruit {
-private:
-	struct element {
-		struct coordinate {
+public:
+	struct element
+	{
+		struct coordinate
+		{
 			int row;
 			int column;
 		} position;
-	} body;
-	bool life;
+	};
+	element body;
+	//bool life;
 
 public:
-	Fruit(World& world)
+	Fruit(void)
 	{
 		// srand(time(NULL));
-		// body.position.column = rand() % 10;
 		// body.position.row = rand() % 10;
+		// body.position.column = rand() % 10;
 
-		body.position.row = 4;
+		body.position.row = 5;
 		body.position.column = 5;
-		world.table[body.position.row * 10 + body.position.column] = '$';
+
+		//life = true;
 	}
 	~Fruit(void)
 	{
 		;
 	}
-	void  generate(World& world)
-	{
-		// srand(time(NULL));
-
-		// в данном стиле если фрукт появится на столе под змейкой, то змейка затрёт
-		// его своими элементами и на следующем цикле условие сгенерирует новый фрукт
-		// поэтому лучше заранее этот случай заблокировать
-		if (world.table[body.position.row * 10 + body.position.column] != '$') {
-			while (world.table[body.position.row * 10 + body.position.column] == 's' || world.table[body.position.row * 10 + body.position.column] == 'S') {
-				body.position.column = rand() % 10;
-				body.position.row = rand() % 10;
-			}
-			world.table[body.position.row * 10 + body.position.column] = '$';
-		}
-	}
-	void  generate(void)
+	void generate(void)
 	{
 		// srand(time(NULL));
 		body.position.row = rand() % 10;
 		body.position.column = rand() % 10;
-		life = true;
+		//life = true;
 	}
-	//bool isCollide(snake, fruit)
+	//bool lifeState(void)
 	//{
-	//	
+	//	return life ? true : false;
 	//}
-
 };
-
+bool collide(Snake& s, Fruit& f)
+{
+	bool result = false;
+	if (
+		s.body[0].position.column == f.body.position.column &&
+		s.body[0].position.row == f.body.position.row)
+	{
+		result = true;
+	}
+	return result;
+}
+bool collide(Snake& s)
+{
+	bool result = false;
+	for (int i = 2; i < s.length; ++i)
+	{
+		if (s.body[0].position.row == s.body[i].position.row &&
+			s.body[0].position.column == s.body[i].position.column)
+		{
+			result = true;
+			s.hit = i;
+		}
+	}
+	return result;
+}
 int main(void)
 {
-	TimeGame tg;
-	Keyboard keyboard;
 	Window window;
+	Keyboard keyboard;
+	TimeGame tg;
 
-	World world(10, 10);
-	Snake snake(world);
-	Fruit fruit(world);
+	//World world(10, 10);
+	//int worldI = 10;
+	//int worldJ = 10;
+
+	Sprite sw('-');
+
+	Snake snake;
+	Sprite sn('s');
+
+	Fruit fruit;
+	Sprite sf('$');
 
 	float timer = 0;
 	float delay = 0.15;
@@ -427,43 +348,99 @@ int main(void)
 			return 0;
 		}
 
+		//if (keyboard.key == Keyboard::keys::p)
+		//{
+		//	Sleep(5000);
+		//}
+
 		snake.setDirection(keyboard.key);
 
 		float elapsed = tg.getElapsedSecondsUpdate();
 		timer += elapsed;
+
 		if (timer >= delay)
 		{
+			// reset timer
 			timer = 0;
 
-			snake.move(world);
+			// body must be update before head
+			snake.updateBody();
 
-			//if ( isCollide(snake, fruit) )
-			//{
-			//	fruit.generate(world);
-			//}
+			// update head and do wrapping
+			switch (snake.direction)
+			{
+			case Snake::Snake::direction::up:
+				snake.body[0].position.row -= 1;
+				if (snake.body[0].position.row < 0)
+				{
+					snake.body[0].position.row = 9;
+				}
+				break;
+			case Snake::direction::down:
+				snake.body[0].position.row += 1;
+				if (snake.body[0].position.row > 9)
+				{
+					snake.body[0].position.row = 0;
+				}
+				break;
+			case Snake::direction::left:
+				snake.body[0].position.column -= 1;
+				if (snake.body[0].position.column < 0)
+				{
+					snake.body[0].position.column = 9;
+				}
+				break;
+			case Snake::direction::right:
+				snake.body[0].position.column += 1;
+				if (snake.body[0].position.column > 9)
+				{
+					snake.body[0].position.column = 0;
+				}
+				break;
+			default:
+				break;
+			}
 
-			fruit.generate(world);
+			// check collision with another object
+			if (collide(snake, fruit))
+			{
+				//Sleep(1000);
+				snake.length++;
+				fruit.generate();
+			}
 
-			//// Если за генерацию фрукта отвечает сам фрукт:
-			//// выглядит логичнее;
-			//// есть необходимость вызова генерации в главном цикле;
-			//// фрукт обязан каждый раз проверять факт существования фрукта;
-			//// Если за генерацию фрукта отвечает змейка:
-			//// новый фрукт появляется в том же кадре, что и поглощение старого фрукта;
-			//// фрукт не обязан проверять факт существования фрукта;
-			//// нет необходимости вызова генерации в главном цикле;
-			//// Второй способ позволяет запретить змейке доступ ко фрукту, но всё равно
-			//// остаётся проблема того, что змейка поглощением изменяет состояние фрукта,
-			//// а значит должна иметь доступ к его данным.
-
+			// check collision with self
+			if (collide(snake))
+			{
+				snake.length = snake.hit;
+				snake.life--;
+			}
 		}
 
 		window.clear();
 
-		world.draw(window);
-		//window.draw(world); // world.draw(window); ?
+		for (int i = 0; i < 10; ++i)
+		{
+			for (int j = 0; j < 10; ++j)
+			{
+				sw.setPosition(i, j);
+				window.draw(sw);
+			}
+		}
+
+		for (int i = 0; i < snake.length; ++i)
+		{
+			sn.setPosition(snake.body[i].position.row, snake.body[i].position.column);
+			window.draw(sn);
+		}
+
+		sf.setPosition(fruit.body.position.row, fruit.body.position.column);
+		window.draw(sf);
+
+		std::cout << " life: " << snake.life << '\n';
 
 		window.display();
 	}
+
 	return 0;
 }
